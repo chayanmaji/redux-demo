babec2aeee0826ece81e17c6a32d1517d28d5c7d
Chayan Maji <chayanmaji@gmail.com>
2022-04-24 00:12:14 -0400

    Implemented basic redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+import store from "./store";
+import * as actions from './actionTypes';
+import { bugAdded, bugResolved, bugRemoved } from './actions';
+
+const unsubscribe = store.subscribe((value)=>{
+    console.log('State changed', store.getState());
+});
+
+console.log('store', store);
+console.log('Initial state', store.getState());
+
+store.dispatch(bugAdded("Bug 1"));
+
+store.dispatch(bugAdded("Bug 2"));
+
+console.log('Current state, after bug created', store.getState());
+
+store.dispatch(bugResolved(1));
+
+store.dispatch(bugResolved(2));
+
+console.log('Current state, after bug resolved', store.getState());
+
+store.dispatch(bugRemoved(1));
+store.dispatch(bugRemoved(2));
+
+console.log('Current state, after bug removed', store.getState());
+
+console.log('Store', store);
